34bded7262c08944fa2e25c05cfafd83376870fd
christopher mcadory <chrmcado2018@iclouud.com>
2023-02-07 11:20:46 -0500

    Initial Commit

---
+++ ssapplecp313/ssapplecp313App.swift
@@ -0,0 +1,17 @@
+//
+//  ssapplecp313App.swift
+//  ssapplecp313
+//
+//  Created by Christopher Mcadory  on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ssapplecp313App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
